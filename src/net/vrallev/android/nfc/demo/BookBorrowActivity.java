fd7a6522a71996daacdb4e436b5b2333405d981e
Lidor <lidorotmazgin@gmail.com>
2014-04-09 19:22:39 +0300

    added book borrow xml+java. TO-DO: in xml:   -add icon for tag scan and 'x' symbol for each item in listview in java: -add asynctask for pulling the title+author for each barcode              -add option to delete items from borrow

---
+++ src/net/vrallev/android/nfc/demo/BookBorrowActivity.java
@@ -0,0 +1,321 @@
+package net.vrallev.android.nfc.demo;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
+import libalg.BranchAndBound;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Lidor on 09/04/14.
+ */
+public class BookBorrowActivity extends Activity {
+
+    public static final String MIME_TEXT_PLAIN = "text/plain";
+    public static final String TAG = "NfcDemo";
+
+    // JSON parser class
+    private JSONParser jsonParser = new JSONParser();
+
+    // username in db url
+    private static final String url_book_borrow = "http://nfclibrary.site40.net/borrow_book_by_barcode.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+
+    Context context;
+    ListView lv;
+    public MySimpleArrayAdapter adapter;
+    ArrayList<Book> b = new ArrayList<Book>();
+
+    Button borrowBtn;
+    private NfcAdapter mNfcAdapter;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book_borrow);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        context = this;
+
+        borrowBtn = (Button) findViewById(R.id.borrowBtn);
+        lv = (ListView) findViewById(R.id.borrowListView);
+        adapter = new MySimpleArrayAdapter(this, b);
+        lv.setAdapter(adapter);
+
+        borrowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new UpdateBorrow().execute();
+            }
+        });
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+		/*
+		 * It's important, that the activity is in the foreground (resumed). Otherwise
+		 * an IllegalStateException is thrown.
+		 */
+        setupForegroundDispatch(this, mNfcAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+		/*
+		 * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+		 */
+        stopForegroundDispatch(this, mNfcAdapter);
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+		/*
+		 * This method gets called, when a new Intent gets associated with the current activity instance.
+		 * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+		 * at the documentation.
+		 *
+		 * In our case this method gets called, when the user attaches a Tag to the device.
+		 */
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+
+            String type = intent.getType();
+            if (MIME_TEXT_PLAIN.equals(type)) {
+
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                new NdefReaderTask().execute(tag);
+
+            } else {
+                Log.d(TAG, "Wrong mime type: " + type);
+            }
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+
+            // In case we would still use the Tech Discovered Intent
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            String[] techList = tag.getTechList();
+            String searchedTech = Ndef.class.getName();
+
+            for (String tech : techList) {
+                if (searchedTech.equals(tech)) {
+                    new NdefReaderTask().execute(tag);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    /**
+     * @param activity The corresponding {@link BaseActivity} requesting to stop the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+
+    public void nfcStatusChanged(View view) {
+        startActivityForResult(new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS),2 );
+    }
+
+    /**
+     * Background task for reading the data. Do not block the UI thread while reading.
+     *
+     * @author Ralf Wondratschek
+     *
+     */
+
+    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
+        @Override
+        protected String doInBackground(Tag... params) {
+            Tag tag = params[0];
+
+            Ndef ndef = Ndef.get(tag);
+            if (ndef == null) {
+                // NDEF is not supported by this Tag.
+                return null;
+            }
+
+            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+
+            NdefRecord[] records = ndefMessage.getRecords();
+            for (NdefRecord ndefRecord : records) {
+                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                    try {
+                        return readText(ndefRecord);
+                    } catch (UnsupportedEncodingException e) {
+                        Log.e(TAG, "Unsupported Encoding", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private String readText(NdefRecord record) throws UnsupportedEncodingException {
+			/*
+			 * See NFC forum specification for "Text Record Type Definition" at 3.2.1
+			 *
+			 * http://www.nfc-forum.org/specs/
+			 *
+			 * bit_7 defines encoding
+			 * bit_6 reserved for future use, must be 0
+			 * bit_5..0 length of IANA language code
+			 */
+
+            byte[] payload = record.getPayload();
+
+            // Get the Text Encoding
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+
+            // Get the Language Code
+            int languageCodeLength = payload[0] & 0063;
+
+            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+            // e.g. "en"
+
+            // Get the Text
+            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                String type = result.substring(0,1);
+                int barcode = Integer.parseInt(result.substring(2));
+
+                if(type.equals("BK")){
+                    super.onPostExecute(result);
+
+                    Book book= new Book();
+                    //update the book array here
+                    book.setBarcode(result.substring(2));
+                    b.add(book);
+
+                    //????//
+                    adapter.notifyDataSetChanged();
+                }
+
+            }
+        }
+    }
+
+
+    class UpdateBorrow extends AsyncTask<String, String, String> {
+
+        /* *
+          * Getting product details in background thread
+          **/
+        protected String doInBackground(String... params) {
+
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    // Check for success tag
+                    int success;
+                    for(int i=0; i< b.size(); i++)
+                    {
+                        try{
+                            List<NameValuePair> params = new ArrayList<NameValuePair>();
+                            params.add(new BasicNameValuePair("barcode", b.get(i).getBarcode().toString()));
+
+                            // getting student details by making HTTP request
+                            // Note that product details url will use GET request
+                            JSONObject json = jsonParser.makeHttpRequest(
+                                    url_book_borrow, "GET", params);
+
+                            Toast.makeText(context, json.toString(), Toast.LENGTH_SHORT).show();
+
+                            // json success tag
+                            if(json!=null) {
+                                success = json.getInt(TAG_SUCCESS);
+                                if (success == 1) {
+                                    // successfully received product details
+                                    //JSONArray productObj = json
+                                    //       .getJSONArray(TAG_PRODUCT); // JSON Array
+
+                                    // get first user object from JSON Array
+                                    //JSONObject product = productObj.getJSONObject(0);
+                                    Toast.makeText(context,"BORROWED",Toast.LENGTH_LONG).show();
+
+                                } else {
+                                    // product with pid not found
+                                }
+                            }
+                        }catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            });
+
+            return null;
+        }
+    }
+}
